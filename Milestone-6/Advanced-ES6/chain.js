7c894e283a757ded057a967733111b5b2a8ee469
Rahe <130391604+rahe01@users.noreply.github.com>
2024-02-25 12:49:15 +0600

    .

--- Milestone-6/Advanced-ES6/chain.js
+++ Milestone-6/Advanced-ES6/chain.js
@@ -1,3 +1,56 @@
-const data = [{id:1, name:"abul", address:'kochu khet'}];
+// const data = [
+//   {
+//     name: "abul",
+//     id: 1,
+//     address: "kochu khet",
+//   },
+// ];
 
-console.log(data[0].address);
+// const result = data[0].name;
+
+// console.log(result);
+
+const products = {
+  count: 5000,
+  data: [
+    {
+      id: 1,
+      name: "HP",
+      price: 35000,
+    },
+
+    {
+      id: 2,
+      name: "Dell",
+      price: 40000,
+    },
+  ],
+};
+
+console.log(products.data[1].price);
+
+const user = {
+  id: 5001,
+  name: "Raj",
+  address: {
+    city: "Bangalore",
+    state: "Karnataka",
+    street: {
+      first: "uttara",
+      second: "path",
+      third: "pune",
+    },
+  },
+};
+
+const user2 = {
+  id: 5002,
+  name: "Raj",
+  address: {
+    city: "Bangalore",
+  },
+};
+
+console.log(user.address.street?.third)
+
+console.log(user2.address.street?.third)
--- Milestone-6/Advanced-ES6/map.js
+++ Milestone-6/Advanced-ES6/map.js
@@ -1,15 +1,3 @@
 const number = [1, 2, 3, 4];
 
-function add(a) {
-  return a*2;
-}
-
-const returnValue  = number.map(add);
-
-console.log(returnValue);
-
-
-const num2 = [1, 2, 3, 4];
-
- const res = num2.map(num = a => a+2)
- console.log(res);
+const result = number.map()
