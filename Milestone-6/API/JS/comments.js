d9510f0aca73b5bad3fb988376a2f4670814cf1d
Rahe <130391604+rahe01@users.noreply.github.com>
2024-02-26 21:58:06 +0600

    .

--- Milestone-6/API/JS/comments.js
+++ Milestone-6/API/JS/comments.js
@@ -7,9 +7,15 @@ const loadComments = () => {
 
 const loadComments2 =  async() => {
 
+ try{
     const res = await fetch("https://jsonplaceholder.typicode.com/comments");
     const data = await res.json();
     console.log(data);
 
+ }
+
+ catch(error) {
+    console.error('data nyy mama');
+ }
 
 }
