9e1bf33a2a9c8a3bfaf1f7f85acda001e8248914
Rahe <130391604+rahe01@users.noreply.github.com>
2024-03-06 23:35:50 +0600

    .

--- Milestone-7/JS-FOR-REACT/storage.js
+++ Milestone-7/JS-FOR-REACT/storage.js
@@ -1,16 +1,17 @@
-localStorage.setItem('Rahe',713715)
-
-const add = ()=>{
-
-    const idInput = document.getElementById('storage-id');
-    const id = idInput.value;
-    const nameInput = document.getElementById('storage-value');
-    const name = nameInput.value;
+localStorage.setItem("Rahe", 713715);
 
 
-    if(id && name){
-        localStorage.setItem(name,id);
-    }
-    idInput.value = '';
-    nameInput.value = '';
-}
+
+const add = () => {
+  const idInput = document.getElementById("storage-id");
+  const id = idInput.value;
+  const nameInput = document.getElementById("storage-value");
+  const name = nameInput.value;
+
+  if (id && name) {
+    localStorage.setItem(name, id);
+  }
+  idInput.value = "";
+  nameInput.value = "";
+
+};
