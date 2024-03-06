d80a3fef100900e4b81e752f71262761b322fa43
Rahe <130391604+rahe01@users.noreply.github.com>
2024-03-06 22:15:32 +0600

    .

--- Milestone-7/JS-FOR-REACT/featch.js
+++ Milestone-7/JS-FOR-REACT/featch.js
@@ -9,10 +9,52 @@ const me = {
 //   const s1 = JSON.parse(studentJson);
 //   console.log(s1);
 
-const key = Object.keys(me);
+// const key = Object.keys(me);
 
-console.log(key);
-const value = Object.values(me);
+// console.log(key);
+// const value = Object.values(me);
 
-console.log(value);
+// console.log(value);
+
+const product = [
+    {
+      name: "laptop",
+      price: 1222,
+      brand: "Hp",
+      color: "silver",
+    },
+    {
+      name: "phone",
+      price: 1222,
+      brand: "realme",
+      color: "silver",
+    },
+    {
+      name: "watch",
+      price: 12223333,
+      brand: "rolax",
+      color: "silver",
+    },
+    {
+      name: "light",
+      price: 1222,
+      brand: "apple",
+      color: "silver",
+    },
   
+  
+  
+  
+  ];
+
+  const newProduct =   {
+    name: "i",
+    price: 1222,
+    brand: "apple",
+    color: "silver",
+  }
+
+  const n = [...product, newProduct];
+  console.log(n);
+
+  console.log(product)
---
+++ Milestone-7/JS-FOR-REACT/shortcut.js
@@ -0,0 +1,25 @@
+// let num =1;
+
+// if(num){
+//     console.log('num is not zero');
+// }
+// else{
+//     console.log('num is zero');
+// }
+
+
+const money =900;
+let food;
+
+if(money>100){
+    food = 'rice';
+}
+else{
+    food ='biskit';
+}
+
+console.log(food);
+
+let food1 = money>100? 'biriyani' : 'cha';
+
+console.log(food1);
