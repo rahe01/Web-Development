d379cab33491d3fe90d859d65a9e78b65ca3ef5e
Rahe <130391604+rahe01@users.noreply.github.com>
2024-03-06 21:53:40 +0600

    .

---
+++ Milestone-7/JS-FOR-REACT/destructring.js
@@ -0,0 +1,20 @@
+const number = [1, 2];
+
+const [x,y] = number;
+
+console.log(x,y);
+
+const product = 
+    {
+      name: "laptop",
+      price: 1222,
+      brand: "Hp",
+      color: ['red', 'green', 'blue'],
+    }
+
+    const [a,b,c] = product.color;
+    console.log(a,b,c);
+
+
+    const {name,age} = {}
+
---
+++ Milestone-7/JS-FOR-REACT/featch.js
@@ -0,0 +1,18 @@
+const me = {
+    name: "Rahe",
+    age: 19,
+  };
+  
+
+//   const studentJson = JSON.stringify(me);
+//   console.log(studentJson);
+//   const s1 = JSON.parse(studentJson);
+//   console.log(s1);
+
+const key = Object.keys(me);
+
+console.log(key);
+const value = Object.values(me);
+
+console.log(value);
+  
