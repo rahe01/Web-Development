691f86b38224f8453b3a4aa360be7e02ff6de005
Rahe <130391604+rahe01@users.noreply.github.com>
2024-03-10 00:22:19 +0600

    Update App.jsx

--- Milestone-7/react-core-concept2/react-app/src/App.jsx
+++ Milestone-7/react-core-concept2/react-app/src/App.jsx
@@ -1,31 +1,30 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
+import "./App.css";
 
-import Counter from './Counter'
-import Team from './Team'
-import Users from './Users';
+import Counter from "./Counter";
+import Team from "./Team";
+import Users from "./Users";
 
-import { Friend }  from './Friend';
+import { Friend } from "./Friend";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  function handleClick(){
-    alert('button clicked')
+  function handleClick() {
+    alert("button clicked");
   }
 
-  const handleClick2 = () =>{
-    alert('button clicked 2')
-  }
+  const handleClick2 = () => {
+    alert("button clicked 2");
+  };
 
-  const addToTwo = (num) =>{
-    alert(num +2);
-  }
+  const addToTwo = (num) => {
+    alert(num + 2);
+  };
 
   return (
     <>
-     
       <h3>React core concept 2</h3>
 
       <Friend></Friend>
@@ -34,19 +33,23 @@ function App() {
 
       <Team></Team>
 
-    <Counter></Counter>
-
+      <Counter></Counter>
 
       <button onClick={handleClick}>Click Me</button>
 
       <button onClick={handleClick2}>Click2</button>
 
-      <button onClick={( )=>{alert('third clicked')}}>Third </button>
+      <button
+        onClick={() => {
+          alert("third clicked");
+        }}
+      >
+        Third{" "}
+      </button>
 
       <button onClick={() => addToTwo(1)}>add</button>
-    
     </>
-  )
+  );
 }
 
-export default App
+export default App;
