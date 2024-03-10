9495289c777df29263c8cbf1e6c5ce5c01a150b7
Rahe <130391604+rahe01@users.noreply.github.com>
2024-03-10 22:32:22 +0600

    country

---
+++ Milestone-7/Countries/react-world-tour/src/components/Country/Country.jsx
@@ -0,0 +1,16 @@
+
+ import './Country1.css'
+const Country = ({country}) => {
+    
+    const {name,flags } = country;
+    
+    return (
+        <div className='country'>
+            <h3>Name : {name?.common}</h3>
+
+            <img src={flags?.png} alt="" />
+        </div>
+    );
+};
+
+export default Country;
