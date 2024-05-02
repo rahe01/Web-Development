a6e1a34ffb288523313a94967fe6e0e49d96ab59
Rahe <130391604+rahe01@users.noreply.github.com>
2024-05-02 19:30:38 +0600

    e

--- Milestone-11/Car-Doctor/CarDoctorClient/Client/src/Routes/Routes.jsx
+++ Milestone-11/Car-Doctor/CarDoctorClient/Client/src/Routes/Routes.jsx
@@ -3,6 +3,9 @@ import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Login from "../pages/Login/Login";
 import SignUp from "../pages/SignUp/SignUp";
+import BookService from "../pages/BookService/BookService";
+import Bookings from "../pages/Bookings/Bookings";
+import PrivateRoute from "./PrivateRoute";
 
 const router = createBrowserRouter([
     {
@@ -20,6 +23,15 @@ const router = createBrowserRouter([
         {
             path: 'signup', 
             element: <SignUp></SignUp>
+        },
+        {
+          path: 'book/:id', 
+          element: <BookService></BookService>, 
+          loader: ({params}) => fetch(`http://localhost:5000/services/${params.id}`)
+        },
+        {
+          path: 'bookings', 
+          element: <PrivateRoute><Bookings></Bookings></PrivateRoute>
         }
       ]
     },
