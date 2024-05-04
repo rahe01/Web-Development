035e2dc38268d9e3ddf2519e35cc25b97dfa5e6c
Rahe <130391604+rahe01@users.noreply.github.com>
2024-05-04 15:52:42 +0600

    r

--- Milestone-11/Car-Doctor/CarDoctorClient/Client/src/pages/Bookings/Bookings.jsx
+++ Milestone-11/Car-Doctor/CarDoctorClient/Client/src/pages/Bookings/Bookings.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
 import BookingRow from "./BookingRow";
+import axios from "axios";
 
 const Bookings = () => {
     const { user } = useContext(AuthContext);
@@ -8,9 +9,13 @@ const Bookings = () => {
 
     const url = `http://localhost:5000/bookings?email=${user?.email}`;
     useEffect(() => {
-        fetch(url)
-            .then(res => res.json())
-            .then(data => setBookings(data))
+        axios.get(url ,{withCredentials: true})
+            .then(data => setBookings(data.data))
+
+
+        // fetch(url)
+        //     .then(res => res.json())
+        //     .then(data => setBookings(data))
     }, [url]);
 
     const handleDelete = id => {
